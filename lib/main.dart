02fd6385d8494d197b6e34672c634f76fdcfd355
Augusto Xavier <augustocbx@gmail.com>
2019-09-04 22:22:51 -0300

    Curso: Criação de Apps Android e iOS com Flutter - Ações de cálculo e reset

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,40 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+  String _infoText = 'Informe seus dados!';
+
+  void _resetFields() {
+    setState(() {
+      weightController.text = '';
+      heightController.text = '';
+      _infoText = 'Informe seus dados!';
+    });
+  }
+
+  void _calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+      print(imc);
+      if (imc < 18.6) {
+        _infoText = "Abaixo do peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = "Peso ideal (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = "Levement acima do peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(3)})";
+      } else {
+        _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(3)})";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +52,7 @@ class _HomeState extends State<Home> {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.refresh),
-            onPressed: () {},
+            onPressed: _resetFields,
           )
         ],
       ),
@@ -34,19 +68,23 @@ class _HomeState extends State<Home> {
               size: 120.0,
             ),
             TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    labelText: 'Peso (kg)',
-                    labelStyle: TextStyle(color: Colors.green)),
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.green, fontSize: 25.0)),
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: 'Peso (kg)',
+                  labelStyle: TextStyle(color: Colors.green)),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 25.0),
+              controller: weightController,
+            ),
             TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    labelText: 'Altura (cm)',
-                    labelStyle: TextStyle(color: Colors.green)),
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.green, fontSize: 25.0)),
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: 'Altura (cm)',
+                  labelStyle: TextStyle(color: Colors.green)),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 25.0),
+              controller: heightController,
+            ),
             Padding(
               padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
               child: Container(
@@ -57,12 +95,12 @@ class _HomeState extends State<Home> {
                     style: TextStyle(color: Colors.white, fontSize: 25.0),
                   ),
                   color: Colors.green,
-                  onPressed: () {},
+                  onPressed: _calculate,
                 ),
               ),
             ),
             Text(
-              'Info',
+              _infoText,
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.green, fontSize: 25.0),
             )
